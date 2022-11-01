b8ce6b81dec779c8c1c4dc8d058925b7451583af
Cristian-Diaz-Developer <Cristian.Frontend.Developer@gmail.com>
2022-10-31 22:42:31 -0500

    Se agrega el CrossOrigin para solicitudes y el EnableMvc (temporal)

--- backend/digitalbooking/src/main/java/com/grupo9/digitalbooking/DigitalBookingApplication.java
+++ backend/digitalbooking/src/main/java/com/grupo9/digitalbooking/DigitalBookingApplication.java
@@ -2,7 +2,9 @@ package com.grupo9.digitalbooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class DigitalBookingApplication {
 
--- backend/digitalbooking/src/main/java/com/grupo9/digitalbooking/controller/CategoryController.java
+++ backend/digitalbooking/src/main/java/com/grupo9/digitalbooking/controller/CategoryController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin (origins = "*")
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
